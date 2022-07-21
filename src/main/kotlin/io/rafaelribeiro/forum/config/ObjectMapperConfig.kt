2829b2b15e657c6c6ecf689abbe37bb5c1784ad4
Rafael Ribeiro <rafael.pr10@gmail.com>
2022-07-21 17:49:32 +0200

    wip

--- src/main/kotlin/io/rafaelribeiro/forum/config/ObjectMapperConfig.kt
+++ src/main/kotlin/io/rafaelribeiro/forum/config/ObjectMapperConfig.kt
@@ -1,6 +1,7 @@
 package io.rafaelribeiro.forum.config
 
 import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,7 +10,9 @@ import org.springframework.context.annotation.Configuration
 class ObjectMapperConfig {
     @Bean
     fun objectMapper(): ObjectMapper {
+        val timeModule = JavaTimeModule()
         return jacksonObjectMapper()
+            .registerModule(timeModule)
             .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
             .disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
