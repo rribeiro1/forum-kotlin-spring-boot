1e6d64a5cee020be233cb7567a32dc808e723044
Rafael Ribeiro <rafael.pr10@gmail.com>
2022-12-14 09:22:54 +0100

    Update project (#343)

--- src/test/kotlin/io/rafaelribeiro/forum/support/AbstractIT.kt
+++ src/test/kotlin/io/rafaelribeiro/forum/support/AbstractIT.kt
@@ -1,17 +1,22 @@
 package io.rafaelribeiro.forum.support
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.rafaelribeiro.forum.support.facades.CourseFacade
 import io.rafaelribeiro.forum.support.facades.RoleFacade
 import io.rafaelribeiro.forum.support.facades.TopicFacade
 import io.rafaelribeiro.forum.support.facades.UserFacade
 import io.restassured.RestAssured
+import io.restassured.config.ObjectMapperConfig
+import io.restassured.config.RestAssuredConfig
 import io.restassured.http.ContentType
+import io.restassured.parsing.Parser
 import io.restassured.specification.RequestSpecification
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.boot.test.web.server.LocalServerPort
+
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
@@ -32,6 +37,9 @@ abstract class AbstractIT {
     @Autowired
     protected lateinit var topicFacade: TopicFacade
 
+    @Autowired
+    protected lateinit var objectMapper: ObjectMapper
+
     protected fun authenticated(): RequestSpecification {
         return RestAssured.given()
             .auth().preemptive().oauth2("Any")
@@ -42,6 +50,12 @@ abstract class AbstractIT {
     @BeforeEach
     fun before() {
         RestAssured.port = testPort
+        RestAssured.defaultParser = Parser.JSON
+        RestAssured.config.objectMapperConfig(
+            ObjectMapperConfig.objectMapperConfig()
+                .jackson2ObjectMapperFactory { _, _ -> objectMapper }
+        )
+
         userFacade.resetDatabase()
         roleFacade.resetDatabase()
         courseFacade.resetDatabase()
